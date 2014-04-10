d8faba6522c3faf6b9dd459af9271c0aab5891b5
jingz8804 <jingz8804@gmail.com>
2014-04-10 09:41:47 -0600

    using pure javascript
    instead of googleVis package

--- ui.R
+++ ui.R
@@ -8,24 +8,38 @@
 library(shiny)
 
 shinyUI(fluidPage(
+    tags$head(
+      tags$link(rel="stylesheet", 
+                type="text/css", 
+                href="http://cdn.leafletjs.com/leaflet-0.7.2/leaflet.css"),
+      tags$script(type="text/javascript",
+                  src="http://cdn.leafletjs.com/leaflet-0.7.2/leaflet.js"),
+      tags$script(type="text/javascript",
+                  src="js/main.js"),
+      tags$script(type='text/javascript',
+                  src='https://www.google.com/jsapi'),
+      tags$script(type="text/javascript",
+                  src="js/geochart.js")
+    ),
     titlePanel("Census Vis"),
     
     sidebarLayout(
       sidebarPanel(
         conditionalPanel(condition="input.condPanels==1",
-                         selectInput(inputId="year", label="Select Year",
-                                     choices=c(2006, 2011)),
                          helpText("Content Panel1")),
         conditionalPanel(condition="input.condPanels==2",
+                         selectInput(inputId="year", label="Select Year",
+                                     choices=c(2006, 2011)),
                          helpText("Content Panel2"))
       ),
       mainPanel(
         tabsetPanel(
-          tabPanel("Canada Census", 
-                   h3(textOutput("year")),
-                   htmlOutput("gvis"),
-                   value=1),
-          tabPanel("Panel 2", value=2),
+          tabPanel("Plot",
+                   div(id="map", style="width: 700px; height: 600px;"),
+          value=1),
+          tabPanel("Summary",
+                   div(id="chart_div", style="width: 900px; height: 500px;"),
+          value=2),
           id="condPanels")
       )
     )
